5623fcc16213aa77ca88dc3ff9ec22307034c01c
gustavo-a-mendes <gust4v0.a.mendes@gmail.com>
2023-08-23 17:14:15 -0300

    incluindo modulo cabeçalho

--- funcionario.c
+++ funcionario.c
@@ -1,1 +1,28 @@
 #include <stdio.h>
+#include "funcionario.h"
+
+struct funcionario {
+  char nome[31];
+  char cargo[101];
+  int documento;
+};
+
+
+Funcionario *func_cria(char* nome, char* cargo, int documento) {
+
+}
+
+
+Funcionario *func_libera(Funcionario* func) {
+
+}
+
+
+void func_ordena(Funcionario** func, int count) {
+
+}
+
+
+void func_salva(Funcionario** func, FILE* fl) {
+  
+}
