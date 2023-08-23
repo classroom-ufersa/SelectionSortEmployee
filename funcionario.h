afe93fe27e664c37d3f61dc43d5b6bc07612c2a8
gustavo-a-mendes <gust4v0.a.mendes@gmail.com>
2023-08-23 17:07:33 -0300

    att funções

--- funcionario.h
+++ funcionario.h
@@ -9,21 +9,21 @@ typedef struct funcionario Funcionario;
 /* Funções Exportadas */
 
 /* Função func_cria
+  cadastra um novo arquivo funcionário, alocando memória e preenchendo os parâmetros
 */
 Funcionario *func_cria(char* nome, char* cargo, int documento);
 
 /* Função func_libera
+  apaga o cadastro de um funcionario, liberando a memória alocada
 */
 Funcionario *func_libera(Funcionario* func);
 
-/* Função func_atribui
-*/
-void func_atribui(int indice, char* nome, char* cargo, int documento);
-
 /* Função func_ordena
+  ordena os funcionários por ordem alfabética, por meio do algoritmo Selection Sort
 */
 void func_ordena(Funcionario** func, int count);
 
 /* Função func_salva
+  guarda dados do tipo estruturado no arquivo
 */
 void func_salva(Funcionario** func, FILE* fl);
