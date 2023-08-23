a358d3ed182413855ae6f8e5ac881dc938a1f9ce
gustavo-a-mendes <gust4v0.a.mendes@gmail.com>
2023-08-23 16:56:17 -0300

    adicionando protótipo das funções

--- funcionario.h
+++ funcionario.h
@@ -1,5 +1,8 @@
 /* TAD: Funcionarios(nome, cargo, documento) */
 
+/* Dependência de módulos */
+#include <stdio.h>
+
 /* Tipo Exportado */
 typedef struct funcionario Funcionario;
 
@@ -7,4 +10,20 @@ typedef struct funcionario Funcionario;
 
 /* Função func_cria
 */
-Funcionario *func_cria(int indice, char* nome, char* cargo, int documento);
+Funcionario *func_cria(char* nome, char* cargo, int documento);
+
+/* Função func_libera
+*/
+Funcionario *func_libera(Funcionario* func);
+
+/* Função func_atribui
+*/
+void func_atribui(int indice, char* nome, char* cargo, int documento);
+
+/* Função func_ordena
+*/
+void func_ordena(Funcionario** func, int count);
+
+/* Função func_salva
+*/
+void func_salva(Funcionario** func, FILE* fl);
