4bbc267cf7b94dbb257d7aec49fdf102d607e66c
gustavo-a-mendes <gust4v0.a.mendes@gmail.com>
2023-08-23 16:42:20 -0300

    cabeçalho funcionario.h

---
+++ funcionario.h
@@ -0,0 +1,10 @@
+/* TAD: Funcionarios(nome, cargo, documento) */
+
+/* Tipo Exportado */
+typedef struct funcionario Funcionario;
+
+/* Funções Exportadas */
+
+/* Função func_cria
+*/
+Funcionario *func_cria(int indice, char* nome, char* cargo, int documento);
