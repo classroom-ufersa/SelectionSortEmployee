9378fe58ec045d6e40ab4a6c9dd9dca4d874fc1d
gustavo-a-mendes <gust4v0.a.mendes@gmail.com>
2023-08-28 19:06:20 -0300

    func_salva implementado pra teste

--- main.c
+++ main.c
@@ -1,71 +1,109 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "funcionario.h"
 
 
-#define MAX_FUNC 3
-
+#define MAX_FUNC 5
 
 int opcoes(void);
 
-
 int main(void){ 
 
-    int opcao, documento, ID;
+    int count_func, ID;
+    int opcao, documento;
     char nome[31], cargo[101];
 
     Funcionario **funcionario = (Funcionario**) calloc(MAX_FUNC,sizeof(Funcionario*));
-    FILE *saida;
+    
+    FILE *dados_func = fopen("arquivo.txt", "rt");
+	if (dados_func == NULL)
+    {
+		printf("Nao foi possivel abrir o arquivo.\n");
+        exit(1);
+	}
 
-    ID = func_leia(funcionario, saida);
+    // ID armazena quantidade inicial de funcionários, para comparação futura:
+    ID = func_leia(funcionario, dados_func);
+    // count_func irá ser usado como indice do vetor de ponteiros:
+    count_func = ID;
 
     do
     {
-
-        opcao = opcoes;
+        opcao = opcoes();
 
         switch (opcao)
         {
-        case 1:
+            case 1:
+                
+                printf("Digite o nome: \n");
+                scanf(" %30[^\n]", nome);
+                printf("Digite o cargo: \n");
+                scanf(" %100[^\n]", cargo);
+                printf("Digite o documento: \n");
+                scanf("%d", &documento);
+                printf("%d", count_func);
+                if(count_func < MAX_FUNC){
+                    funcionario[count_func] = func_cadastra(nome, cargo, documento);
+                    count_func++;
+                }else{
+                    printf("Maximo de funcionarios atingido!");
+                }
+
+                break;
+            case 2:
+
+                break;
             
-            printf("Digite o nome: \n");
-            scanf(" %30[^\n]", nome);
-            printf("Digite o documeno: \n");
-            scanf("%d", &documento);
-            printf("Digite o cargo: \n");
-            scanf(" %100[^\n]", cargo);
+            case 3:
+                break;
 
-            if(ID <= MAX_FUNC){
-                funcionario[ID] = func_cadastra(nome, cargo, documento);
-                ID++;
-            }else{
-                printf("Maximo de funcionarios atingido!");
-            }
+            case 4:
+                char resp;
+                printf("\nEncerrando Programa...\n");
+                if (count_func > ID) {
+                    printf("\nHa funcionarios cadastrados que ainda nao foram salvos.");
+                    printf("\nDeseja Salvar [S/N]? ");
+                    scanf(" %c", &resp);
+                    
+                    if (toupper(resp) == 'S')
+                    {
+                        func_salva(funcionario, dados_func, count_func);
+                    }
+                }
+                printf("\nPrograma Encerrado!\n");
+                break;
 
-            break;
-        
-        default:
-            break;
+            default:
+                printf("\nOpcao Invalida! Tente novamente.\n");
+                break;
         }
+    } while (opcao != 4);
 
-    } while (opcao < 4 && opcao > 0);
-    
-
-
+    func_libera(funcionario, count_func);
+    fclose(dados_func);
 
     return 0;
+}
+
+void menu(Funcionario** func, FILE* fl, int count) {
+    
+    
+
+    
 }
 
 int opcoes(void){
 
     int opcao;
 
-    printf("1 - Cadastar funcionario");
-    printf("2 - ....");
-    printf("3 - ....");
-    printf("4 - Encerrar");
-
+    printf("\n1 - Cadastar funcionario");
+    printf("\n2 - ....");
+    printf("\n3 - ....");
+    printf("\n4 - Encerrar");
+    printf("\nEscolha uma opcao: ");
+    scanf("%d", &opcao);
     return opcao;
 
 }
--- teste.c
+++ teste.c
@@ -10,25 +10,27 @@ Aluno *alunos[10];
 
 int main(void) {
 
-  int i;
-  for (i = 0; i < 5; i++) {
-    alunos[i] = (Aluno*)malloc(sizeof(Aluno));
-    scanf(" %20[^\n]", alunos[i]->nome);
-    scanf("%d", &alunos[i]->matricula);
-  }
+  // int i;
+  // for (i = 0; i < 5; i++) {
+  //   alunos[i] = (Aluno*)malloc(sizeof(Aluno));
+  //   scanf(" %20[^\n]", alunos[i]->nome);
+  //   scanf("%d", &alunos[i]->matricula);
+  // }
   
 
-  FILE* arquivo = fopen("arquivo.txt", "at");
+  FILE* arquivo = fopen("arquivo.txt", "rt");
   if (arquivo == NULL) exit(1);
   else printf("Arquivo aberto com sucesso!");
   
-  for (i = 0; i < 5; i++) {
-    fprintf(arquivo, "%-20s\t%d\n", alunos[i]->nome, alunos[i]->matricula);
-  }
+  fseek(arquivo, 0, SEEK_END);
+  printf("%ld", ftell(arquivo));
+  // for (i = 0; i < 5; i++) {
+  //   fprintf(arquivo, "%-20s\t%d\n", alunos[i]->nome, alunos[i]->matricula);
+  // }
   
-  for (i = 0; i < 5; i++) {
-    free(alunos[i]);
-  }
+  // for (i = 0; i < 5; i++) {
+  //   free(alunos[i]);
+  // }
   fclose(arquivo);
   
 
